eee2bee60ef31257c5d68eb680200faa48ede83d
Tun Sreynouch <sengn5112@gmail.com>
2024-08-26 14:18:12 +0700

    I removed all content jsx on the ServicesPart component

--- src/components/SubComponents/ServicesPart/ServicesPart.jsx
+++ src/components/SubComponents/ServicesPart/ServicesPart.jsx
@@ -3,14 +3,6 @@
 
 export default function ServicesPart(){
     return(
-        <div className='services'>
-            <div className='header'>
-                <h1>Services</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, dolor? Porro cum id quasi fuga ipsam reprehenderit nihil tenetur enim iure? Officiis assumenda explicabo unde dolor. Facere nam laboriosam debitis.</p>
-            </div>
-            <div className='content'>
-            
-            </div>
-        </div>
+       
     )
 }
